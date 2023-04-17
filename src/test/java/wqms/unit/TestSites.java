5f55deef34ea4b4b5cdf85a371acf965a0318c32
Till Kaiser <art0s@mailbox.org>
2023-04-17 22:31:28 +0200

    refactor(tests): refactored test site url's for resuse in multiple tests

---
+++ src/test/java/wqms/unit/TestSites.java
@@ -0,0 +1,9 @@
+package wqms.unit;
+
+public final class TestSites {
+	 final static String wikipediaUrl = "http://www.wikipedia.org";
+	 final static String errorUrl = "hadsasdo3o2342ß00ß#ä#ä+";
+	 final static String oracleUrl = "https://www.oracle.com/de/";
+	 final static String siteWithoutMetadata = "http://chat.openai.de/";
+}
+
