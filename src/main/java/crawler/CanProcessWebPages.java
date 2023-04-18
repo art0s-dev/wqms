f7ff724d827de31820f8be5b7fd70ff4a3018055
Till Kaiser <art0s@mailbox.org>
2023-04-18 21:31:00 +0200

    refactor(crawler): refactored scan method to default implementation for reuse

--- src/main/java/crawler/CanProcessWebPages.java
+++ src/main/java/crawler/CanProcessWebPages.java
@@ -1,9 +1,37 @@
 package crawler;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 import java.util.List;
+import java.util.Optional;
 
 public interface CanProcessWebPages {
 	public static List<Meta> crawl(String url) {
 		return null;
+	}
+	
+	public default Optional<HttpResponse<String>> scan(String url){
+		try {
+			HttpClient client = HttpClient.newBuilder()
+					.version(Version.HTTP_1_1)
+					.followRedirects(Redirect.NORMAL)
+					.connectTimeout(Duration.ofSeconds(10))
+					.build();
+			
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(url))
+					.build();
+			
+			var response = client.send(request, BodyHandlers.ofString());	
+			return Optional.of(response);	
+		} catch(Exception e) {
+			return Optional.empty();
+		}
 	}
 }
