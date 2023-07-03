29cb39565479a1440274fccd1b4ed2fa6910c525
Till Kaiser <art0s@mailbox.org>
2023-07-03 19:49:40 +0200

    update(Sitemap): updated current code

--- src/main/java/main/Sitemap.java
+++ src/main/java/main/Sitemap.java
@@ -24,13 +24,24 @@ public class Sitemap {
 	public static List<String> create(URL url) throws 
 	ParserConfigurationException,
 	IOException, 
-	SAXException, 
+	SAXException,
 	SAXParseException,
 	Exception
 	{
 		var factory = SchemaFactory.newInstance(asXSD);
+		factory.setErrorHandler(null);
 		InputStream stream = url.openStream();
 		var streamSource = new StreamSource(stream);
+		
+		/**
+		 * Validator fails with even the smallest error
+		 * every not compatible version produces an error 
+		 * and if the document uses http in the xmlns namespace 
+		 * declaration it produces a SAXException
+		 * 
+		 * TODO: Implement 0.84 version backwards compatible and see
+		 * if the error still occures
+		 */
 		
 		Function<String,Boolean> checkValidity = (filePath) -> {
 			try {
@@ -42,8 +53,15 @@ public class Sitemap {
 			}
 		};
 		
-		var xmlIsASitemap = checkValidity.apply("schemes/sitemap.xsd.xml");
-		var xmlIsASiteindex = checkValidity.apply("schemes/siteindex.xsd.xml");
+		/**cvc-complex-type.2.4.a: Ungültiger Content 
+		wurde beginnend mit Element 
+		'{"http://www.sitemaps.org/schemas/sitemap/0.9":lastmod}' 
+		 '{"http://www.sitemaps.org/schemas/sitemap/0.9":priority,
+		"http://www.sitemaps.org/schemas/sitemap/0.9"]}' wird erwartet.
+		*/
+		
+		var xmlIsASitemap = checkValidity.apply("schemes/sitemap_v9.xsd.xml");
+		var xmlIsASiteindex = checkValidity.apply("schemes/siteindex_v9.xsd.xml");
 		if(!xmlIsASitemap && ! xmlIsASiteindex) {
 			throw new Exception("Sitmap is not valid");
 		}
@@ -53,6 +71,35 @@ public class Sitemap {
 		DocumentBuilder builder = instance.newDocumentBuilder();
 		builder.setErrorHandler(null);
 		Document parsedSitemap = builder.parse(stream);
+		
+		
+		/**
+		 * There are 2 possible outcomes - either 
+		 * the link is a sitemap or a siteindex in the best
+		 * case i can parse just one sitemap or 
+		 * in the worst case i have to iterate over the siteindex
+		 * and parse every sitemap. in both cases i parse a sitemap
+		 */
+		Function<String, List<String>> parseSitemap = (link) -> {
+			return List.of();
+		};
+		
+		/*
+		 * if (sitemap) {
+		 * 	return parseSitemap(link);
+		 * }
+		 * 
+		 * List<String> linkList = List.of();
+		 * for(sitemap){
+		 * 	linkList.add(parseSitemap(link))
+		 * }
+		 * 
+		 * return linkList
+		 * 	
+		 */
+		
+		
+		
 		
 		return List.of();
 	}
--- src/test/java/unit/SitemapTest.java
+++ src/test/java/unit/SitemapTest.java
@@ -2,18 +2,23 @@ package unit;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import main.Sitemap;
 
 class SitemapTest {
 	
-	@Test
+	@Test @Disabled
 	void GivenUrlWithNoSitemap_WhenLinkListIsCalled_ThenThrow() 
 	throws MalformedURLException {
 		URL url = new URL("https://www.microsoft.com");
@@ -21,15 +26,12 @@ class SitemapTest {
 		assertTrue(e.getClass() == Exception.class);
 	}
 	
-	@Test @Disabled
-	void GivenUrlWithOnlyOneSitemapAndItsDamaged_WhenLinkListIsCalled_ThenThrow() {}
-	
-	@Test @Disabled
-	void GivenUrlWithMoreThanOneSitemapsAndOneIsDamaged_ThenLinkListIsCalled_ThenOmit() {}
-	
-	@Test @Disabled
-	void GivenUrlToLocalSiteindex_WhenLinkListIsCalled_ThenParseToLinkList() {}
-	
-	@Test @Disabled
-	void GivenUrlToLocalSitemap_WhenLinkListIsCalled_ThenParseToLinkList() {}
+	@Test
+	void GivenUrlToLocalSitemap_WhenLinkListIsCalled_ThenParseToLinkList()
+	throws SAXParseException, ParserConfigurationException, IOException, SAXException, Exception {
+		URL url = new URL("https://www.oracle.com/applications.xml");
+		List<String> sitemap = Sitemap.create(url);
+		assertTrue(sitemap.get(2) == "https://www.oracle.com/applications/applications-unlimited/");
+	}
 }
+
